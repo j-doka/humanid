3e69da34a252f2faf70364e95b4e63d5228f9da0
j-doka <jdoka42@gmail.com>
2023-04-18 14:10:30 +0100

    removed bad bg and started on title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,17 @@
 import Image from 'next/image'
-// import { Inter } from 'next/font/google'
-
-// const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between p-24">
+      <div className="bg-[#363636] px-6 rounded-full">
+        <h1 className="text-3xl">Human 
+          <span className='text-[#58CBA4]'>.id</span>
+        </h1>
+      </div>
       
-      <h1>human <span className='text-[#58CBA4]'>.id</span></h1>
 
       {/* date label */}
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center pt-24">
         <h2 className="text-2xl font-bold">Enter Birthdate</h2>
 
         <div className="flex flex-row items-center justify-center mt-4">
